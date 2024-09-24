eeeb92fe5d721d81b1a73ef8a5620fb74a38f64a
Avinash <avinashkmg1310@gmail.com>
2024-09-24 17:59:57 +0530

    mid sections

--- src/components/UI/Buttons.jsx
+++ src/components/UI/Buttons.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Buttons = ({ label, iconURL }) => {
+const Buttons = ({ label, iconURL, className }) => {
 	return (
-		<button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full border-coral-red">
+		<button
+			className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full border-coral-red ${className}`}
+		>
 			{label}
-			<img
-				src={iconURL}
-				alt={label}
-				className="ml-2 rounded-full w-5 h-5 "
-			/>
+			{iconURL && (
+				<img
+					src={iconURL}
+					alt={label}
+					className="ml-2 rounded-full w-5 h-5 "
+				/>
+			)}
 		</button>
 	);
 };
--- src/components/sections/SpecialOffer.jsx
+++ src/components/sections/SpecialOffer.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { offer } from "../../assets/images";
+import Buttons from "../UI/Buttons";
+import { arrowRight } from "../../assets/icons";
 
 const SpecialOffer = () => {
-	return <div>SpecialOffer</div>;
+	return (
+		<section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+			<div>
+				<img
+					src={offer}
+					width={773}
+					height={987}
+					className="object-contain w-full"
+				/>
+			</div>
+
+			<div className="flex flex-1 flex-col">
+				<h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-sm">
+					<span className="text-coral-red"> Special </span> Offer
+				</h2>
+
+				<p className="mt-4 lg:max-w-lg info-text">
+					Embark on a shopping journey that redefines your experience
+					with unbeatable deals. From premier selections to incredible
+					savings, we offer unparalleled value that sets us apart.
+					<br />
+					<br />
+					Navigate a realm of possibilities designed to fulfill your
+					unique desires, surpassing the loftiest expectations. Your
+					journey with us is nothing short of exceptional.
+				</p>
+
+				<div className="mt-11 flex flex-wrap gap-5">
+					<Buttons label="Shop Now" iconURL={arrowRight} />
+
+					<Buttons
+						label="Learn more"
+						className="bg-white border border-slate-gray text-slate-gray"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SpecialOffer;
